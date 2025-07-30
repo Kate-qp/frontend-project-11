1f2495b559f002058093262f3c888cd52c9e6fe6
Kate-qp <kpark4328@gmail.com>
2025-07-30 22:35:16 +0300

    Исправлены ошибки ESLint и тесты

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,6 @@
-// eslint.config.js
 import js from '@eslint/js'
 import stylistic from '@stylistic/eslint-plugin'
 import globals from 'globals'
-import prettierPlugin from 'eslint-plugin-prettier'
-import prettierConfig from 'eslint-config-prettier'
 
 export default [
   {
@@ -18,26 +15,14 @@ export default [
     },
     plugins: {
       '@stylistic': stylistic,
-      prettier: prettierPlugin,
     },
     rules: {
       '@stylistic/comma-dangle': 'off',
       '@stylistic/arrow-parens': 'off',
       '@stylistic/semi': 'off',
-      'prettier/prettier': [
-        'error',
-        {
-          printWidth: 100,
-          singleQuote: true,
-          trailingComma: 'all',
-          arrowParens: 'always',
-          semi: false,
-        },
-      ],
     },
   },
   js.configs.recommended,
-  prettierConfig,
   {
     ignores: ['node_modules/**', '*.min.js'],
   },
